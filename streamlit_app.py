5e53b7b7522108f352ccdb5b291c04243e2c7c0a
Shivaraj A <109541049+Shivraj-24@users.noreply.github.com>
2025-05-26 17:33:19 +0000

    Added a comment to show what is search value

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,16 @@ st.write("The name on the Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write(f'The search value for {fruit_chosen}, is {search_on}.')
         st.subheader(f"{fruit_chosen} Nutrition information")
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
